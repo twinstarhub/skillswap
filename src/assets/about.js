c66593b4174b2d124dfe0f27c07324759b643dd1
twinstarhub <golddev9697@gmail.com>
2022-01-07 21:56:06 -0800

    Add about assets for pages

---
+++ src/assets/about.js
@@ -0,0 +1,17 @@
+export const aboutData = [
+  {
+    head: "Combat Scams",
+    text: `
+    SkillSwap is a great way to combat scams in Web3 as it eliminates the need for a third-party to mediate between buyers and sellers. This ensures that buyers and sellers are interacting directly with each other and that there is no middleman taking a cut or skewing the terms of the agreement. Additionally, by utilizing the blockchain and smart contracts, all transactions are secured, transparent, and traceable, further reducing the risk of fraud. This means that buyers are protected from being scammed and sellers are assured of getting paid.`,
+  },
+  {
+    head: "Peer To Peer",
+    text: `
+    SkillSwap reduces the risk of exploitation, as freelancers can work directly with their clients and have more control over their work environment and pay rate. Unlike  Fiverr or Upwork, we do not own or act like we own freelancers on SkillSwap. You are free to do as you please. Our percentage (7.5%) is lower as well and needs to be paid by the buyer and will not be taken from the seller.`,
+  },
+  {
+    head: "Escrow Payments",
+    text: `
+    The escrow payment feature on SkillSwap is a great benefit for both freelancers and employers. For freelancers, it ensures that payment is received for completed work. For employers, it ensures that work is completed to their satisfaction before payment is released. The use of escrow also reduces the likelihood of fraud by ensuring that funds are held until the project is completed. This helps to protect both parties and build trust between them.`,
+  },
+];
