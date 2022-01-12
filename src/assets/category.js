d7f299220c45bcdbd3cd200e718f78f9b99af93b
twinstarhub <golddev9697@gmail.com>
2022-01-11 21:58:47 -0800

    Add category assets

---
+++ src/assets/category.js
@@ -0,0 +1,127 @@
+import art from "../images/art.png";
+import consultation from "../images/consultation.png";
+import laptop from "../images/laptop.png";
+import meme from "../images/meme.png";
+import art1 from "../images/art1.png";
+import coach from "../images/coach.png";
+import hand from "../images/hand.png";
+import twitter from "../images/twitter.png";
+import video from "../images/video.png";
+import expand from "../images/expand.png";
+import code from "../images/code.png";
+import scam from "../images/scam.png";
+import nftGene from "../images/nftGene.png";
+import translator from "../images/translator.png";
+import discord from "../images/discord.svg";
+import webdesign from "../images/webdesign.svg";
+
+export const categoryData = [
+  {
+    img: code,
+    head: "Developer",
+    text: "A Web3 Developer builds, for example, decentralized applications (dApps) and smart contracts using blockchain technologies! ",
+    link: "Developer",
+  },
+  {
+    img: art1,
+    head: "Artist",
+    text: "An NFT (collection) artist creates any type of digital artwork that you need and some artists can be hired to create your entire collection!",
+    link: "Artist",
+  },
+  {
+    img: art,
+    head: "Art Director",
+    text: "Oversees the visual style of your NFT Collection and social media art!",
+    link: "Art director",
+  },
+  {
+    img: coach,
+    head: "Project Manager",
+    text: "A Project Manager is responsible for planning, organizing, and directing the completion of a specific project within the allocated time and budget!",
+    link: "Project manager",
+  },
+  {
+    img: consultation,
+    head: "NFT Specialist / Adviser",
+    text: "A NFT Specialist / Adviser provides advice and guidance to clients on the best strategies for your NFT collection in most areas!",
+    link: "NFT Specialist / Adviser",
+  },
+  {
+    img: hand,
+    head: "Collabs Manager",
+    text: "A Collaboration Manager facilitates the collaboration and coordination of WL allocation and partnerships between NFT projects!",
+    link: "Collabs Manager",
+  },
+  {
+    img: discord,
+    head: "Discord Moderator",
+    text: "A Discord Moderator is responsible for enforcing rules and keeping the community safe and friendly!",
+    link: "Discord Moderator",
+  },
+  {
+    img: expand,
+    head: "Influencer",
+    text: "An Influencer has the power to affect the purchase decisions of others through their promotion of a product or service and can help you grow your social presence!",
+    link: "Influencer",
+  },
+  {
+    img: laptop,
+    head: "Community Manager",
+    text: "A Community Manager helps to build and maintain relationships between a business and its customers!",
+    link: "Community Manager",
+  },
+  {
+    img: twitter,
+    head: "Social Media Manager",
+    text: "A Social Media Manager creates, curates, and manages content on social media channels to build an engaged audience and drive brand awareness!",
+    link: "Social Media Manager",
+  },
+  {
+    img: meme,
+    head: "Meme Creator",
+    text: "A Meme Creator creates humorous visual images, typically with text, to share on social media for comedic effect!",
+    link: "Meme Creator",
+  },
+  {
+    img: video,
+    head: "NFT Animator",
+    text: "An NFT Animator animates your NFTs and brings them to life!",
+    link: "NFT Animator",
+  },
+  {
+    img: webdesign,
+    head: "Web UI/UX Designer",
+    text: " A UI/UX Designer creates user-friendly, visually appealing, and intuitive designs to enable the best possible user experience!",
+    link: "Web Designer",
+  },
+  {
+    img: discord,
+    head: "Discord Server Specialist",
+    text: "A Discord Server Specialist is responsible for building, managing and maintaining a Discord Server, ensuring that it is optimized for user engagement and performance!",
+    link: "Discord Server Specialist",
+  },
+  {
+    img: nftGene,
+    head: "NFT Generators",
+    text: "NFT Generators will generate your NFT collection Art variations with metadata, rarities and exclusions!",
+    link: "NFT Generators",
+  },
+  {
+    img: discord,
+    head: "Alpha Hunter",
+    text: "A Alpha hunter seeks to identify and take advantage of profit opportunities in the Crypto / Non Fungible Token market and helps users make informed decisions about investing in Crypto and NFTs!",
+    link: "Alpha Hunter",
+  },
+  {
+    img: translator,
+    head: "Writing and Translation",
+    text: "White Paper, Lore, Website Content, Social Content, Creative Writing, Slogans, Brand Stories, Brand Voice & Tone and more!",
+    link: "Writing and Translation",
+  },
+  {
+    img: scam,
+    head: "Scam Consultant",
+    text: "A Scam Consultant is a Cyber Security Specialist. This section is also ideal for a former scammer who wishes to discontinue their involvement in fraudulent activity and assist projects in avoiding being scammed!",
+    link: "Scam Consultant",
+  },
+];
